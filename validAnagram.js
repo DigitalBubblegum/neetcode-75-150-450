13c220a977370407a523d11260be483da9ac2958
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-12-21 14:56:50 +0530

    -valid anagrams

---
+++ validAnagram.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  const a = s.split("").sort().join("");
+  const b = t.split("").sort().join("");
+  if (a === b) {
+    return true;
+  }
+  return false;
+};
