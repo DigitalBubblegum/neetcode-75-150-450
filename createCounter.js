a36b897612a52cdd4da49c0058e4caaff84809b6
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-12-31 07:53:50 +0530

    -2620. Counter faster solution

--- createCounter.js
+++ createCounter.js
@@ -21,3 +21,21 @@ var createCounter = function (n) {
  * counter() // 11
  * counter() // 12
  */
+
+
+/// FASTER SOLUTION
+
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    return () => n++
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
