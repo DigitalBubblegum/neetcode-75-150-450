ceeae8d676cee4901778b062a61bf7632657b3ce
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2024-01-04 19:38:05 +0530

    -557. Reverse Words in a String III

--- reverseWords.js
+++ reverseWords.js
@@ -2,11 +2,11 @@
  * @param {string} s
  * @return {string}
  */
-var reverseWords = function(s) {
-   let newS = s.split(' ')
-   let ret = []
-   for(let i=0;i<newS.length;i++){
-       ret.push(newS[i].split('').reverse().join(''))
-   }
-   return ret.join(' ')
+var reverseWords = function (s) {
+  let newS = s.split(" ");
+  let ret = [];
+  for (let i = 0; i < newS.length; i++) {
+    ret.push(newS[i].split("").reverse().join(""));
+  }
+  return ret.join(" ");
 };
