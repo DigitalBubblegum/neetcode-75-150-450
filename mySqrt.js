b30c36d1b5ad4eb3286d1df29c832db805132e98
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2024-01-11 17:31:13 +0530

    -69. Sqrt(x)

---
+++ mySqrt.js
@@ -0,0 +1,8 @@
+//This is not really the correct solution. need to find a proper fix
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    return Math.floor(x**(1/2))
+};
