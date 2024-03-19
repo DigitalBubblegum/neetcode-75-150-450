ab4efe75b0b9de5ca677992ef83efd43e47caa9b
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2024-03-19 09:01:01 +0530

    -283. Move Zeroes //modified with two pointer for O(n) TC

--- moveZeroes.js
+++ moveZeroes.js
@@ -13,3 +13,37 @@ var moveZeroes = function (nums) {
     }
   }
 };
+
+//O(N) Solution with the help of two pointers
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    if(nums.length<=1){
+        return nums
+    }
+    let l = 0
+    let r = 1
+    while(r<nums.length){
+        if(nums[l]!=0 && nums[r]===0){
+            r++
+            l++
+            continue
+        }
+        if(nums[l]!=0 && nums[r]!==0){
+            r++
+            l++
+            continue
+        }
+        if(nums[l]===0 && nums[r]!==0){
+            let temp = nums[r]
+            nums[r] = nums[l]
+            nums[l] = temp
+            r++
+            l++
+            continue
+        }
+        r++
+    }
+};
